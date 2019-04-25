d6a322579b0b8b154a834f20049df66b8ac3dd53
Sahilsoni94600 <49449269+Sahilsoni94600@users.noreply.github.com>
2019-04-26 01:43:18 +0530

    Add files via upload

---
+++ Output_Presentation.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+#include <unistd.h>  //Header file for sleep(). 
+#include <pthread.h>
+
+int main()
+{
+      printf(" \n");
+	  printf(" \n");
+	  printf("\t\t\t\t ================================================= \n");
+	  printf("\t\t\t\t||                                               || \n");
+	  printf("\t\t\t\t||\t   ___      ____    ____   ______   \t ||\n");
+	  printf("\t\t\t\t||\t .'   `.   |_   \\  /   _|.' ____ \\   \t || \n");
+	  printf("\t\t\t\t||\t/  .-.  \\    |   \\/   |  | (___ \\_|   \t ||\n");
+	  printf("\t\t\t\t||\t| | \\ | |    | |\\  /| |   _.____`.    \t ||\n");
+	  printf("\t\t\t\t||\t\\  `-'  \\_  _| |_\\/_| |_ | \\____) |   \t ||\n");
+	  printf("\t\t\t\t||\t `.___.\\__||_____||_____| \\______.'   \t ||\n");
+	  printf("\t\t\t\t||                                               || \n");
+	  sleep(1); 
+	  printf(" \t\t\t\t||\t  ___(Query Management System)___\t ||\n");
+	  printf("\t\t\t\t ================================================= \n");
+	  sleep(1); 
+	  system("COLOR 17");
+	  sleep(1); 
+	  system("COLOR 47");
+	  sleep(1); 
+	  system("COLOR 60");
+	  printf(" ---------------------------------------------------------------------------------------------------------------------");
